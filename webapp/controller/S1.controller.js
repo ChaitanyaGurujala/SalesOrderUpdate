916529f729edf26a8ad4cfa5ce01a996aede78cc
Chaitanya Gurujala <chaitanya.gurujala@gmail.com>
2024-09-03 16:28:21 +0000

    First Push 9-2-2024

---
+++ webapp/controller/S1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("salesorderupdate.controller.S1", {
+        onInit: function () {
+
+        }
+    });
+});
